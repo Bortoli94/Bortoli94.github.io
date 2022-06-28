0750038d6cab36b6adcc33d386fb401161965d92
bortoli94 <gbortoli.rosario@hotmail.com>
2022-06-28 02:24:56 -0300

    primer commit

---
+++ js/pages/index.js
@@ -0,0 +1,24 @@
+//************************ ELEMENTOS DOM ****************
+const selectCity = document.getElementById("SelectCity"); //selector de ciudades
+const consultButton = document.getElementById("consult"); //boton de consulta clima
+//*******************************************************
+
+function addCity() {
+    let cities = getCitiesFromLocalStorage();
+    if (cities.length == 0) {
+        selectCity.innerHTML += `<option value="" disabled selected>Debe cargar ciudades</option>`
+    }
+    else {
+        selectCity.innerHTML += `<option value="" disabled selected>Seleccionar Ciudad</option>`
+        for (let i = 0; i < cities.length; i++) {
+            selectCity.innerHTML += `<option value="${cities[i]}">${cities[i]}</option>`
+        }
+    }
+}
+
+consultButton.onclick = function (){
+    consultAPI(selectCity.value);
+}
+
+addCity();
+
